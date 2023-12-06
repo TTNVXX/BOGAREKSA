240af40f055b989ed904a5df3f4d0be8e7e18620
Rahadyan Rizqy <rahadyan@rdnet.id>
2023-12-06 14:56:09 +0700

    Delete generate.py

--- generate.py
+++
@@ -1,6 +0,0 @@
-import random
-
-def generatePrivateUniqueId(adder=""):
-    characters = 'qwertyuiopasdfghjklzxcvbnm' + '1234567890'
-    unique_id = ''.join(random.choices(characters, k=10))
-    return adder+"-"+unique_id if len(adder) > 0 else unique_id
